63fd939ac6f17772f00a47f61e1aa91e8005c8fb
RadicaIEdward <edward.o.galindez@gmail.com>
2022-10-26 18:12:43 -0400

    Complete assignment
    Refactor code
    Implement Error Checking Extra Credit
    Complete README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ESLint Configuration
+
+Functionality:
+This is a time zone converter app for Eastern Standard Time.
+
+Instructions:
+1. Open Terminal
+2. Enter "node tz.js <Country/City_Name> for the country you wish to conver the timezone to.
+3. To format the output in a more readable format, include the --format tag
+
+Example:
+![App in Use](images/in-use.png)
--- tz.js
+++ tz.js
@@ -1,21 +1,28 @@
 const yargs = require('yargs');
 const moment = require('moment-timezone');
 
+const commandArray = yargs.argv._;
 const command = yargs.argv._[0];
 const params = yargs.argv;
 
 moment.tz.setDefault('America/New_York');
-
+const defaultTime = ('America/New_York');
+const allTimezones = moment.tz.names();
 let targetTimezone;
 
-if (Object.keys(params).length < 2) {
+if (commandArray.length === 0) {
   console.log('Usage: node <script-file> <timezone>');
   process.exit(1);
 } else {
   targetTimezone = command;
-  if (params.format) {
+  if (allTimezones.includes(targetTimezone) === false) {
+    console.log('This timezone does not exist in our database. Please try again.');
+    process.exit(2);
+  } else if (params.format) {
+    console.log(`The time here is: ${moment().tz(defaultTime).format('dddd, MMMM Do YYYY, h:mm:ss a')}`);
     console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format('dddd, MMMM Do YYYY, h:mm:ss a')}`);
-    process.exit(1);
+    process.exit(0);
   }
+  console.log(`The time here is: ${moment().tz(defaultTime).format()}`);
   console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
 }
