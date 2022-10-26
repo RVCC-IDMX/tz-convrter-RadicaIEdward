64bbf0a17fff65f64ec538503f287d6011b17b06
RadicaIEdward <edward.o.galindez@gmail.com>
2022-10-26 17:13:58 -0400

    Implement functionality using yargs

--- tz.js
+++ tz.js
@@ -1,29 +1,21 @@
 const yargs = require('yargs');
-
-console.log(yargs.argv);
+const moment = require('moment-timezone');
 
 const command = yargs.argv._[0];
 const params = yargs.argv;
-
-// if (command === 'fly') {
-//   console.log('flying');
-// } else if (command === 'drive') {
-//   if (params.speed && params.car) { console.log(`driving ${params.car} at ${params.speed} mph`); } else {
-//     console.log('driving');
-//   }
-// }
-
-const moment = require('moment-timezone');
 
 moment.tz.setDefault('America/New_York');
 
 let targetTimezone;
 
-if (yargs.argv.length < 3) {
+if (Object.keys(params).length < 2) {
   console.log('Usage: node <script-file> <timezone>');
   process.exit(1);
 } else {
-  targetTimezone = yargs.argv._[0];
+  targetTimezone = command;
+  if (params.format) {
+    console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format('dddd, MMMM Do YYYY, h:mm:ss a')}`);
+    process.exit(1);
+  }
+  console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
 }
-
-console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
